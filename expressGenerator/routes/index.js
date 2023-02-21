01b3ac9dfce228d56e1357a64cdedbc42bef884c
Gokulcortex <123944489+Gokulcortex@users.noreply.github.com>
2023-02-21 13:38:43 +0530

    Update index.js

--- expressGenerator/routes/index.js
+++ expressGenerator/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("comming gv")
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express Gv apllication' });
 });
 
 module.exports = router;
